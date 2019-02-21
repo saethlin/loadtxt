594d6cb194b8ebed1c07f7faa29b7afd7586f1fa
Ben Kimock <kimockb@gmail.com>
2019-02-20 20:38:57 -0500

    beginning refactor to multiple files

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,9 @@
 use std::ffi::{CStr, CString};
 use std::fs;
 use std::os::raw::{c_char, c_int};
+use std::sync::atomic::{AtomicBool, Ordering};
+
+mod checked;
 
 #[derive(Default, Clone)]
 struct Chunk<T> {
@@ -22,114 +25,34 @@ pub unsafe extern "C" fn loadtxt(
     skiprows: c_int,
     rows: *mut u64,
     cols: *mut u64,
-    has_error: *mut u8,
-    error_line: *mut u64,
+    error: *mut c_char,
 ) -> *const f64 {
     *rows = 0;
     *cols = 0;
-    *has_error = 0;
+    *error = std::ptr::null();
     *error_line = 0;
 
     let filename = CStr::from_ptr(filename).to_str().unwrap();
     let comments = CStr::from_ptr(comments).to_str().unwrap();
 
-    let ncpu = num_cpus::get();
-
-    let contents = fs::read_to_string(filename).unwrap();
-    // handle skiprows
-    let remaining = contents
-        .splitn(skiprows as usize + 1, '\n')
-        .last()
-        .unwrap()
-        .trim();
-
-    let first_line = remaining.lines().next().unwrap();
-    let num_cols = first_line.split_whitespace().count();
-    *cols = num_cols as u64;
-    let approx_rows = remaining.len() / first_line.len();
-    let chunksize = remaining.len() / ncpu;
-
-    let mut parsed_chunks = vec![Chunk::default(); ncpu];
-    // Divide into chunks for threads
-    scoped_threadpool::Pool::new(ncpu as u32).scoped(|scoped| {
-        let mut slice_begin = 0;
-        for e in &mut parsed_chunks {
-            let mut slice_end = slice_begin + chunksize;
-            if slice_end > remaining.len() {
-                slice_end = remaining.len();
-            } else {
-                while !remaining.is_char_boundary(slice_end)
-                    || remaining.as_bytes()[slice_end] != b'\n'
-                {
-                    slice_end += 1;
-                    if slice_end == remaining.len() {
-                        break;
-                    }
-                }
-            }
-
-            let slice = &remaining[slice_begin..slice_end];
-            scoped.execute(move || {
-                // Cannot use enumerate on rows or these_cols because they must
-                // outlive their iterators
-                let mut error_line = None;
-                let mut rows = 0;
-                let mut data = Vec::with_capacity((approx_rows * num_cols * 2) / ncpu);
-                slice
-                    .trim()
-                    .lines()
-                    .filter(|l| !l.starts_with(comments))
-                    .for_each(|l| {
-                        let mut these_cols = 0;
-                        l.split_whitespace().for_each(|s| {
-                            these_cols += 1;
-                            match s.parse() {
-                                Ok(v) => data.push(v),
-                                Err(_) => {
-                                    if error_line.is_none() {
-                                        error_line = Some(rows)
-                                    }
-                                }
-                            };
-                        });
-                        if these_cols != num_cols {
-                            if error_line.is_none() {
-                                error_line = Some(rows)
-                            }
-                        }
-                        rows += 1;
-                    });
-                *e = Chunk {
-                    data,
-                    rows,
-                    error_line,
-                }
-            });
-
-            slice_begin = slice_end;
+    let data = checked::loadtxt_checked(filename, comments, skiprows);
+    
+    match data {
+        Ok(d) => {
+            let ptr = data.as_ptr();
+            std::mem::forget(data);
+            ptr
         }
-    });
-
-    let mut data =
-        Vec::with_capacity(parsed_chunks.iter().map(|c| c.data.len()).sum::<usize>() + 1);
-    for chunk in parsed_chunks {
-        data.extend_from_slice(&chunk.data);
-        if let Some(line) = chunk.error_line {
-            if *has_error == 0 {
-                *error_line = line + *rows;
-                *has_error = 1;
-            }
+        Err(e) => {
+            let error_string = CString::from(e.to_string());
+            std::mem::forget(error_string);
+            *error = error_string.as_ptr();
+            std::ptr::null()
         }
-        *rows += chunk.rows as u64;
     }
-
-    let ptr = data.as_ptr();
-    std::mem::forget(data);
-    ptr
 }
 
 use std::io;
-use std::sync::atomic::{AtomicBool, Ordering};
 fn unchecked_internal<T>(filename: &str) -> io::Result<RustArray<T>>
 where
     T: Clone + Send + lexical::FromBytes + Sync + Default,
