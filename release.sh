9e9150b011acc2cf3053462864bd71cf03259db7
Ben Kimock <kimockb@gmail.com>
2019-10-31 08:24:04 -0400

    remove actions/checkout

--- release.sh
+++ release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-git clone https://github.com/saethlin/loadtxt
+git clone --depth=1 https://github.com/saethlin/loadtxt
 cd loadtxt
 python3 setup.py bdist_wheel
-python3 -m auditwheel repair dist/loadtxt*.whl -w /wheelhouse
+python3 -m auditwheel repair dist/*.whl -w /wheelhouse
